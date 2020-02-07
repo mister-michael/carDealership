ee195cb0d7d7a5bff8a772e7439e8338682eafaf
Michael Clark <michaelclarknashville@protonmail.com>
2020-02-07 16:29:02 -0600

    completed first two steps of challenge, working on 3

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,89 @@ const API = {
     }
 }
 
- API.getCarData()
+API.getCarData()
     .then(arr => {
-        const carsFrom2017 = arr.filter(object => object.vehicle.year === 2017)
-        console.log("filtered out 2017s", carsFrom2017)
-        const profitsFrom2017 =  carsFrom2017.map(arrayObject => arrayObject.gross_profit)
-        console.log("array of profits for 2017 cars", profitsFrom2017)
-        const profitFrom2017Reduced = profitsFrom2017.reduce((a, b) => a + b)
-        console.log("total profit 2017", profitFrom2017Reduced)
-    })
 
+        const wholeArray = arr
+
+        const carsSold2017 = wholeArray.filter(object => object.purchase_date.startsWith("2017"))
+        // console.log("sales date startsWith 2017", carsSold2017)
+
+        const mapProfits = carsSold2017.map(object => object.gross_profit)
+        // console.log("gross_profits 2017)", mapProfits)
+
+        const totalProfits = mapProfits.reduce((a, b) => a + b, 0)
+        console.log("2017 total profits", totalProfits)
+
+        //which month sold the most
+
+        const salesInMonth = (month) => {
+            const monthArray = carsSold2017.filter(object => object.purchase_date.startsWith(`2017-${month}`))
+            // console.log(`monthArray ${month}`, monthArray)
+            if (monthArray.length !== 0) {
+                const profitArrayFromMonth = monthArray.map(object => object.gross_profit)
+                // console.log(`profitArrayFromMonth ${month}`, profitArrayFromMonth)
+
+                const monthTotal = profitArrayFromMonth.reduce((a, b) => a + b)
+                // console.log(`monthTotal ${month}`, monthTotal)
+                return {
+                    "month": month,
+                    "totalProfit": monthTotal
+                }
+            } else {
+                return {
+                    "month": month,
+                    "totalProfit": 0
+                }
+            }
+        }
+
+        const yearSalesByMonthArray = [salesInMonth("01"), salesInMonth("02"), salesInMonth("03"), salesInMonth("04"), salesInMonth("05"), salesInMonth("06"), salesInMonth("07"), salesInMonth("08"), salesInMonth("09"), salesInMonth("10"), salesInMonth("11"), salesInMonth("12")];
+
+        // console.log("salesByMonth", yearSalesByMonthArray)
+
+        const profitByMonthMapped = yearSalesByMonthArray.map(month => month.totalProfit)
+        // console.log("profitByMonthMapped", profitByMonthMapped)
+
+        const profitByMonthSorted = yearSalesByMonthArray.map(month => month.totalProfit).sort((a, b) => b - a)
+
+        // console.log("is yearSalesByMonthArray sorted?", yearSalesByMonthArray)
+
+
+        const findMonthHigh = (unsortedArray) => {
+
+            for (let i = 0; i < 12; i++) {
+
+                if (profitByMonthSorted[0] === unsortedArray[i]) {
+                    // console.log("profitByMonthMapSorted", profitByMonthSorted)
+                    return `highest sales were in month-${i + 1} of $${profitByMonthMapped[i]}`
+                }
+            }
+        }
+        console.log(findMonthHigh(profitByMonthMapped));
+
+        const sales_idArray = wholeArray.map(object => object.sales_id)
+        console.log(sales_idArray)
+
+        const sales_idArrayUnique = new Set(sales_idArray)
+        console.log(sales_idArrayUnique)
+
+        const totalSalesById = (salesPersonArray) => {
+            let salesArrArray = []
+
+            for (let i = 0; i < salesPersonArray.length; i++) {
+
+                let salesArray = []
+
+                if (salesPersonArray[i] === wholeArray.sales_id) {
+                    salesArray.push(object => object.gross_profit)
+                }
+
+                
+            }
+            
+        }
+
+        console.log(totalSalesById(sales_idArrayUnique))
+
+    })
